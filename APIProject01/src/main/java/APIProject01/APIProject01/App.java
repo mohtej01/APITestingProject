691db02dc3a2b6d1a0485fcb7cc009b2f30f0b9e
mohtej01 <mohit.tejwani@protiviti.co.in>
2019-12-12 11:05:47 +0530

    First Commit

---
+++ APIProject01/src/main/java/APIProject01/APIProject01/App.java
@@ -0,0 +1,13 @@
+package APIProject01.APIProject01;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
